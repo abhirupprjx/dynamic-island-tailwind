4f1b8609029217db51d7c5d604f92861253f8fe2
abhirupprjx <adatta@mondee.com>
2025-02-20 10:39:32 +0530

    updated JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 
 function App() {
-  const btns = [
-    "Compact",
-    "Expanded"
-  ]
+ 
   return (
     <div className="flex gap-2">
-      {
-          btns.map((b)=>(
-            <button key={b} className="bg-red-200 hover:bg-red-100 p-4 rounded-full cursor-pointer"  onClick={()=> console.log("pressed", b)}>{b}</button>
-
-          ))
-      }
+        <button  className="bg-red-200 hover:bg-red-100 p-4 rounded-full cursor-pointer" 
+         onClick={()=> console.log("pressed")}>Default notification</button>
       </div>
   )
 }
