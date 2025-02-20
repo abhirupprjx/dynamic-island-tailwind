6a28af28ebc669696116886efa7c95333af72305
abhirupprjx <adatta@mondee.com>
2025-02-20 14:42:28 +0530

    update Notification transition width, height

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -1,7 +1,10 @@
 import { X } from 'lucide-react';
+import { useRef, useState } from 'react';
 
 const Notification = ({open, onOpenChange})=>{
-  
+  const [expanded, setExpanded] = useState(false);
+
+  const ref = useRef(null);
   if(!open){
     return null;
   }
@@ -9,14 +12,27 @@ const Notification = ({open, onOpenChange})=>{
   return (
     <div className="absolute top-5  left-1/2 -translate-x-1/2">
 
-        <div className="bg-red-400 min-w-sm p-2 rounded-full cursor-pointer">
+        <div className={`bg-red-400 max-w-sm rounded-3xl cursor-pointer`} onClick={()=> setExpanded(!expanded)}>
 
-            <div className='flex justify-between items-center'>
-            <div className="flex items-center gap-2">
-                <img className="w-8 h-8 rounded-full" src="https://fastly.picsum.photos/id/435/200/200.jpg?hmac=yk7-HtvV0x2Z6OB4YhbyAbYxX0nQQCNTzs_MgegSkcE" />
-                <p className="text-white">New message received</p> 
-            </div>
-            <X className="w-4 h-4 text-white cursor-pointer" onClick={()=>onOpenChange(false)}/>
+           <div className='p-2'>
+                <div className='flex justify-between items-center'>
+                    <div className="flex items-center gap-2">
+                        <img className="w-8 h-8 rounded-full" src="https://fastly.picsum.photos/id/435/200/200.jpg?hmac=yk7-HtvV0x2Z6OB4YhbyAbYxX0nQQCNTzs_MgegSkcE" />
+                        <p className="text-white">New message received</p> 
+                    </div>
+                    <X className="w-4 h-4 text-white cursor-pointer shrink-0" onClick={()=>onOpenChange(false)}/>
+                </div>
+
+                <div className='overflow-y-hidden transition-all'
+                style={{height: expanded ? ref.current?.scrollHeight || 0 : 0}}
+                ref={ref}
+                >
+                    <div className='pb-20 px-2'>
+                    You have a new message from John Doe. Click to read.
+                    More content....
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
